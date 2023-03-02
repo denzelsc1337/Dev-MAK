805ef1690ad5ec599e32746a1fef2e327071dac1
denzelsc1337 <denzelsotomayor@gmail.com>
2023-03-02 17:04:41 -0500

    update dashboard

--- index_.php
+++ index_.php
@@ -13,18 +13,18 @@ session_start();
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="Vista/plugins/fontawesome-free/css/all.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="Vista/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="Vista/dist/css/adminlte.min.css">
 </head>
-<body class="hold-transition sidebar-mini  sidebar-collapse layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img src="images/mak_2.png" alt="AdminLTELogo" height="90" width="15%">
+    <img class="animation__wobble" src="Vista/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -169,7 +169,7 @@ session_start();
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="Vista/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">MAK</span>
     </a>
 
@@ -178,7 +178,7 @@ session_start();
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="Vista/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block"><?php echo $_SESSION['nom_usu'] ?></a>
@@ -204,7 +204,7 @@ session_start();
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
-              <i src="images/mak.png"></i>
+              <i src="Vista/images/mak.png"></i>
               <p>
                 Servicios
                 <i class="right fas fa-angle-left"></i>
@@ -333,49 +333,49 @@ session_start();
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/UI/general.html" class="nav-link">
+                <a href="Vista/pages/UI/general.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>General</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/icons.html" class="nav-link">
+                <a href="Vista/pages/UI/icons.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Icons</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/buttons.html" class="nav-link">
+                <a href="Vista/pages/UI/buttons.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Buttons</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/sliders.html" class="nav-link">
+                <a href="Vista/pages/UI/sliders.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Sliders</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/modals.html" class="nav-link">
+                <a href="Vista/pages/UI/modals.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Modals & Alerts</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/navbar.html" class="nav-link">
+                <a href="Vista/pages/UI/navbar.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Navbar & Tabs</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/timeline.html" class="nav-link">
+                <a href="Vista/pages/UI/timeline.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Timeline</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/UI/ribbons.html" class="nav-link">
+                <a href="Vista/pages/UI/ribbons.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Ribbons</p>
                 </a>
@@ -392,25 +392,25 @@ session_start();
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/forms/general.html" class="nav-link">
+                <a href="Vista/pages/forms/general.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>General Elements</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/forms/advanced.html" class="nav-link">
+                <a href="Vista/pages/forms/advanced.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Advanced Elements</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/forms/editors.html" class="nav-link">
+                <a href="Vista/pages/forms/editors.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Editors</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/forms/validation.html" class="nav-link">
+                <a href="Vista/pages/forms/validation.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Validation</p>
                 </a>
@@ -433,13 +433,13 @@ session_start();
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/tables/data.html" class="nav-link">
+                <a href="Vista/pages/tables/data.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>DataTables</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="pages/tables/jsgrid.html" class="nav-link">
+                <a href="Vista/pages/tables/jsgrid.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>jsGrid</p>
                 </a>
@@ -450,7 +450,7 @@ session_start();
           <!--MAK MODULOS-->
           <li class="nav-header">SERVICIOS</li>
           <li class="nav-item">
-            <a href="seleccion_servicio.php" class="nav-link">
+            <a href="Vista/seleccion_servicio.php" class="nav-link">
               <i class="nav-icon fas fa-calendar-alt"></i>
               <p>
                 Seleccion de Servicio
@@ -870,9 +870,9 @@ session_start();
               </div>
               <!-- /.card-header -->
                 <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="images/search.jpg" alt="Dist Photo 1">
+                  <img class="card-img-top" src="Vista/images/search.jpg" alt="Dist Photo 1">
                   <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <a href="#" class="btn btn-block btn-primary">INICIAR</a>
+                    <button type="button" class="btn btn-block btn-primary">INICIAR</button>
                   </div>
                 </div>
               <!-- /.card-body -->
@@ -890,10 +890,9 @@ session_start();
               </div>
               <!-- /.card-header -->
                 <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="images/money.jpg" alt="Dist Photo 1">
+                  <img class="card-img-top" src="Vista/images/money.jpg" alt="Dist Photo 1">
                   <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <a href="#" class="btn btn-block btn-primary">INICIAR</a>
-
+                    <button type="button" class="btn btn-block btn-primary">INICIAR</button>
                   </div>
                 </div>
               <!-- /.card-body -->
@@ -911,9 +910,9 @@ session_start();
               </div>
               <!-- /.card-header -->
                 <div class="card mb-2 bg-gradient-dark">
-                  <img class="card-img-top" src="images/finance.jpg" alt="Dist Photo 1">
+                  <img class="card-img-top" src="Vista/images/finance.jpg" alt="Dist Photo 1">
                   <div class="card-img-overlay d-flex flex-column justify-content-end">
-                    <a href="#" class="btn btn-block btn-primary">INICIAR</a>
+                    <button type="button" class="btn btn-block btn-primary">INICIAR</button>
                   </div>
                 </div>
               <!-- /.card-body -->
@@ -946,26 +945,26 @@ session_start();
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="Vista/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="Vista/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="Vista/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.js"></script>
+<script src="Vista/dist/js/adminlte.js"></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="plugins/raphael/raphael.min.js"></script>
-<script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<script src="Vista/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="Vista/plugins/raphael/raphael.min.js"></script>
+<script src="Vista/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="Vista/plugins/jquery-mapael/maps/usa_states.min.js"></script>
 <!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="Vista/plugins/chart.js/Chart.min.js"></script>
 
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="Vista/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard2.js"></script>
+<script src="Vista/dist/js/pages/dashboard2.js"></script>
 </body>
 </html>
