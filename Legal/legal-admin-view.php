128e376b5aa0cd6e1b8f6ecf97a8bb04d7757bf7
denzelsc1337 <denzelsotomayor@gmail.com>
2023-08-03 12:47:37 -0500

    Update legal-admin-view.php

--- Legal/legal-admin-view.php
+++ Legal/legal-admin-view.php
@@ -3,12 +3,11 @@
 $data_legal_json = isset($_GET['data_legal']) ? $_GET['data_legal'] : '';
 $data_legal = json_decode($data_legal_json, true);
 
-echo "<pre>";
 //print_r($data_legal_json);
 //print_r($data_legal_json);
 //print_r($data_legal);
-echo $data_legal['nom_client'];
-echo "</pre>";
+//echo $data_legal['nom_client'];
+
 
 if (isset($data_legal_json['nom_client']) && isset($data_legal_json['ape_client'])) {
     $nombre_apellido = $data_legal_json['nom_client'] . ' ' . $data_legal_json['ape_client'];
