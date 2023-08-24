c0842a39a3f2e82ea4ee72cec00e788a787329ee
Arroshi <victor.arroyo0302@gmail.com>
2023-08-24 09:33:15 -0500

    .

--- Vista/nav_bar_moduls.php
+++ Vista/nav_bar_moduls.php
@@ -20,12 +20,6 @@
 
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4 mak-bg">
-  <!-- Brand Logo -->
-  <a href="../dashboard.php" class="brand-link mak-bg">
-    <img src="../Vista/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">MAK</span>
-  </a>
-
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
--- dashboard.php
+++ dashboard.php
@@ -34,6 +34,9 @@ require_once('config/security.php');
     <nav class="main-header navbar navbar-expand navbar-dark mak-bg">
       <!-- Left navbar links -->
       <ul class="navbar-nav">
+        <a href="../dashboard.php" class="d-flex align-items-center mak-bg">
+          <img src="Vista/images/mak.png" alt="AdminLTE Logo" class="brand-image" style="width: 70px;">
+        </a>
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
@@ -57,12 +60,6 @@ require_once('config/security.php');
 
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4 mak-bg">
-      <!-- Brand Logo -->
-      <a href="dashboard.php" class="brand-link mak-bg">
-        <img src="Vista/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">MAK</span>
-      </a>
-
       <!-- Sidebar -->
       <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
